f1714840a2476af193dda0adfd1fb3990c1b56ca
Sam Sutherland <samsut@xam.com.au>
2023-10-07 17:46:42 +1100

    Clear down MAUI template

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SemanticKernel;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
